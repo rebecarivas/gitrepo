f2fff704f13f71f01e12e4ae918af4fe0b0e9454
rebecarivas <rebecarivas@gmail.com>
2024-06-17 15:28:13 -0300

    api axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-import axios from axios;
+import axios from "axios";
 
-const api = axios.create({baseURL: "https://api.github.com"},);
+const api = axios.create({ baseURL: "https://api.github.com" });
 
 export default api;

37f231562a3a2db514ce8af8331531c2d22fc7ea
rebecarivas <rebecarivas@gmail.com>
2024-06-26 09:16:20 -0300

    cria a funcao para requisicao dos detalhes do repositorio dentro o

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from "../../services/api";
 
 function Repositorio() {
+  const [repoOwner, setRepoOwner] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { repositorio } = useParams();
-  console.log(repositorio);
 
+  useEffect(() => {
+    async function requestRepo() {
+      //faz as duas requisições ao mesmo tempo dentro de um array
+      const [repoData, issueData] = await Promise.all([
+        api.get(`/repos/${repositorio}`),
+        api.get(`/repos/${repositorio}/issues`, {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        }),
+      ]);
+      setRepoOwner(repoData.data);
+      setIssues(issueData.data);
+      setLoading(false);
+    }
+
+    requestRepo();
+  }, [repositorio]);
   return (
     <>
       <h1 style={{ color: "#fff" }}>Repositório: {repositorio}</h1>
